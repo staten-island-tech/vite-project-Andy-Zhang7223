988ee80b67f5457cb7ac0db29e7d036e4d3e0b6a
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-11-19 12:32:37 -0500

    11/19/2025

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { setupCounter } from "./counter.js";
+import { setupmodes } from "./modesbtn.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>Welcome To The Worst Meme Generator!</h1>
+    <h2>~in the worldddd!~</h2>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="modesbtn" type="button></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
+setupmodes(document.querySelector("#modesbtn"));
