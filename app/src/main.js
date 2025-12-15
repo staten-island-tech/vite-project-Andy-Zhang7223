73066c735fc8002d366c8e12a9dec78ab831cd3b
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-12-14 20:41:17 -0500

    12/14/2025

--- app/src/main.js
+++ app/src/main.js
@@ -1,43 +1,48 @@
 import "./style.css";
+// import "./modesbtn.js";
 
 const MemeTemplates = [
-  { name: "Drake Hotline Bling", img: "https://i.imgflip.com/4/30b1gx.jpg" },
-  {
-    name: "Gru Plan making",
-    img: "https://cdn-useast1.kapwing.com/static/templates/grus-plan-meme-template-full-94c95233.webp",
-  },
+  { name: "Drake Hotline Bling", img: "https://i.imgflip.com/4/30b1gx.jpg", category: "Comparing"},
   {
     name: "Disappointed Man",
     img: "https://a.pinatafarm.com/775x500/a2c9247953/disappointed-guy.jpg",
+    category: "Comparing"
   },
   {
     name: "The office, they're the same",
     img: "https://en.meming.world/images/en/c/c4/They%E2%80%99re_The_Same_Picture.jpg",
+    category: "Comparing"
   },
   {
     name: "Pikachu shocked",
     img: "https://www.vice.com/wp-content/uploads/sites/2/2020/04/1588176174179-download-1.jpeg",
+    category: "Reaction"
   },
   {
     name: "___ or draw 25",
     img: "https://www.tjtoday.org/wp-content/uploads/2021/01/IMG_7504.jpg",
+    category: "Reaction"
   },
-  { name: "I'm once again asking", img: "https://i.imgflip.com/4/3oevdk.jpg" },
+  { name: "I'm once again asking", img: "https://i.imgflip.com/4/3oevdk.jpg", category: "Misc."},
   {
     name: "Difficult choices",
     img: "https://images.wondershare.com/filmora/article-images/best-meme-templates-04.jpg",
+    category: "Comparing"
   },
   {
     name: "Wow! This is worthless!",
     img: "https://supermeme-space-prd.ams3.cdn.digitaloceanspaces.com/templates/This-is-Worthless.png",
+    category: "Reaction"
   },
   {
     name: "My heart when...",
     img: "https://pbs.twimg.com/media/E9Ub8UpWQAgEUMW.jpg",
+    category: "Reaction"
   },
   {
     name: "Shooting from the Moon",
     img: "https://i.pinimg.com/736x/48/61/86/48618695046dd9f6ed4949f024a220f0.jpg",
+    category: "Misc."
   },
 ];
 
@@ -45,12 +50,24 @@ function inject(memes) {
   const Templates = document.querySelector(".Templates");
   Templates.insertAdjacentHTML(
     "afterbegin",
-    `<div class="templateselect">
+    `<div class="templateselect" data-category="${memes.category}">
     <img class="img" src=${memes.img} alt="">
-    <input class="selectbtn" value="Select" type="submit" size="100%">
+    <input class="selectbtn" value="Select" type="submit">
     </div>`
   );
 }
+
+function injectplaceholders() {
+  const memepreview = document.querySelector(".memepreview");
+  memepreview.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="previewimgdiv">
+    <img class="previewimg" src="" alt="This is where your meme will be!">
+    </div>`
+  )
+}
+
+injectplaceholders()
 
 MemeTemplates.forEach((MemeTemplate) => inject(MemeTemplate));
 
@@ -81,6 +98,17 @@ function selectTemplate() {
     const previewimgdiv = document.querySelector(".previewimgdiv");
     previewimgdiv.appendChild(clone)
   });
+  const Imageupload = document.querySelector(".upload");
+  Imageupload.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+      }
+      reader.readAsDataURL(file);
+    }
+  })
 }
 
 selectTemplate();
@@ -91,7 +119,6 @@ function FirstInputs() {
     "afterbegin",
     `<button class="Addinputs">Add inputs</button>
     <button class="Nopeinputs">Remove inputs</button>
-    <input type="file" size="20" value="Choose file to use as template">
     <div class="Actualinputs">
       <input class="someinputs" type="text" size="30">
       <input class="someinputs" type="text" size="30">
@@ -102,8 +129,6 @@ function FirstInputs() {
 }
 
 function putinputonimg() {
-  // const inputsread = document.querySelector(".someinputs");
-  // const text = inputsread.value
   const previewimgdiv = document.querySelector(".previewimgdiv");
   const imgcheck = document.querySelector(".previewimg");
   const inputsindiv = document.querySelectorAll(".someinputs");
@@ -111,10 +136,6 @@ function putinputonimg() {
   if (imgcheck) {
     inputsindivArray.forEach((inputs) =>{
       const words = inputs.value
-      const oldwords = document.querySelector(".input")
-      if (oldwords){
-        oldwords.remove();
-      }
       previewimgdiv.insertAdjacentHTML(
         "beforeend",
         `<p class="input">${words}</p>`
@@ -132,14 +153,38 @@ function runningputinputsonimg() {
     btn.addEventListener("click", function () {
       const oldtext = document.querySelectorAll(".input");
       const oldtextArray = Array.from(oldtext);
-      oldtextArray.forEach((oldtext)=> function(){
+      oldtextArray.forEach((oldtext) => {
       if (oldtext){
         oldtext.remove();
       }
-      putinputonimg();
     })
+    putinputonimg();
     })
 }
 
 FirstInputs();
 runningputinputsonimg();
+
+function filtering(btncategory) {
+  const templates = document.querySelectorAll(".templateselect");
+  templates.forEach((template) => {
+    const templateCategory = template.getAttribute("data-category").toLowerCase();
+    if (templateCategory === btncategory || btncategory === "all") {
+      template.style.display = "";
+    } else {
+      template.style.display = "none";
+    }
+  });
+}
+
+function filteringbtns() {
+  const filterbtns = document.querySelectorAll(".filters");
+  filterbtns.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      const buttoncategory = event.target.textContent.toLowerCase();
+      filtering(buttoncategory);
+    });
+  });
+}
+
+filteringbtns();
