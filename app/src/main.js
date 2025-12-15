fc5def43d59d296c5fa3dbc7525fdee4ae6df8d5
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-12-15 12:32:57 -0500

    12/15/2025

--- app/src/main.js
+++ app/src/main.js
@@ -1,48 +1,55 @@
 import "./style.css";
-// import "./modesbtn.js";
 
 const MemeTemplates = [
-  { name: "Drake Hotline Bling", img: "https://i.imgflip.com/4/30b1gx.jpg", category: "Comparing"},
+  {
+    name: "Drake Hotline Bling",
+    img: "https://i.imgflip.com/4/30b1gx.jpg",
+    category: "Comparing",
+  },
   {
     name: "Disappointed Man",
     img: "https://a.pinatafarm.com/775x500/a2c9247953/disappointed-guy.jpg",
-    category: "Comparing"
+    category: "Comparing",
   },
   {
     name: "The office, they're the same",
     img: "https://en.meming.world/images/en/c/c4/They%E2%80%99re_The_Same_Picture.jpg",
-    category: "Comparing"
+    category: "Comparing",
   },
   {
     name: "Pikachu shocked",
     img: "https://www.vice.com/wp-content/uploads/sites/2/2020/04/1588176174179-download-1.jpeg",
-    category: "Reaction"
+    category: "Reaction",
   },
   {
     name: "___ or draw 25",
     img: "https://www.tjtoday.org/wp-content/uploads/2021/01/IMG_7504.jpg",
-    category: "Reaction"
+    category: "Reaction",
   },
-  { name: "I'm once again asking", img: "https://i.imgflip.com/4/3oevdk.jpg", category: "Misc."},
+  {
+    name: "I'm once again asking",
+    img: "https://i.imgflip.com/4/3oevdk.jpg",
+    category: "Misc.",
+  },
   {
     name: "Difficult choices",
     img: "https://images.wondershare.com/filmora/article-images/best-meme-templates-04.jpg",
-    category: "Comparing"
+    category: "Comparing",
   },
   {
     name: "Wow! This is worthless!",
     img: "https://supermeme-space-prd.ams3.cdn.digitaloceanspaces.com/templates/This-is-Worthless.png",
-    category: "Reaction"
+    category: "Reaction",
   },
   {
     name: "My heart when...",
     img: "https://pbs.twimg.com/media/E9Ub8UpWQAgEUMW.jpg",
-    category: "Reaction"
+    category: "Reaction",
   },
   {
     name: "Shooting from the Moon",
     img: "https://i.pinimg.com/736x/48/61/86/48618695046dd9f6ed4949f024a220f0.jpg",
-    category: "Misc."
+    category: "Misc.",
   },
 ];
 
@@ -64,10 +71,10 @@ function injectplaceholders() {
     `<div class="previewimgdiv">
     <img class="previewimg" src="" alt="This is where your meme will be!">
     </div>`
-  )
+  );
 }
 
-injectplaceholders()
+injectplaceholders();
 
 MemeTemplates.forEach((MemeTemplate) => inject(MemeTemplate));
 
@@ -84,9 +91,9 @@ function selectTemplate() {
       oldimg.remove();
     }
     const clone = img.cloneNode(false);
-    clone.classList.remove("img")
+    clone.classList.remove("img");
     clone.classList.add("previewimg");
-    const oldpreviewimgdiv = document.querySelector(".previewimgdiv")
+    const oldpreviewimgdiv = document.querySelector(".previewimgdiv");
     if (oldpreviewimgdiv) {
       oldpreviewimgdiv.remove();
     }
@@ -96,19 +103,30 @@ function selectTemplate() {
       </div>`
     );
     const previewimgdiv = document.querySelector(".previewimgdiv");
-    previewimgdiv.appendChild(clone)
+    previewimgdiv.appendChild(clone);
   });
   const Imageupload = document.querySelector(".upload");
-  Imageupload.addEventListener('change', function () {
+  Imageupload.addEventListener("change", function () {
     const file = this.files[0];
+    let oldimg = document.querySelector(".memepreview img");
+    if (oldimg) {
+      oldimg.remove();
+    }
+    memepreview.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="previewimgdiv">
+      <img class="" src="" alt="">
+      </div>`
+    );
     if (file) {
+      const previewimg = document.querySelector(".previewimg");
       const reader = new FileReader();
-      reader.onload = function(e) {
-        preview.src = e.target.result;
-      }
+      reader.onload = function (e) {
+        previewimg.src = e.target.result;
+      };
       reader.readAsDataURL(file);
     }
-  })
+  });
 }
 
 selectTemplate();
@@ -117,11 +135,9 @@ function FirstInputs() {
   const inputsdiv = document.querySelector(".Templates");
   inputsdiv.insertAdjacentHTML(
     "afterbegin",
-    `<button class="Addinputs">Add inputs</button>
-    <button class="Nopeinputs">Remove inputs</button>
-    <div class="Actualinputs">
-      <input class="someinputs" type="text" size="30">
-      <input class="someinputs" type="text" size="30">
+    `<div class="Actualinputs">
+      <input class="someinputs1" type="text" size="30">
+      <input class="someinputs2" type="text" size="30">
     </div>
     <button class="printinput">Put On Meme</button>
     `
@@ -131,35 +147,33 @@ function FirstInputs() {
 function putinputonimg() {
   const previewimgdiv = document.querySelector(".previewimgdiv");
   const imgcheck = document.querySelector(".previewimg");
-  const inputsindiv = document.querySelectorAll(".someinputs");
-  const inputsindivArray = Array.from(inputsindiv);
+  const inputsindiv1 = document.querySelector(".someinputs1");
+  const inputsindiv2 = document.querySelector(".someinputs2");
   if (imgcheck) {
-    inputsindivArray.forEach((inputs) =>{
-      const words = inputs.value
-      previewimgdiv.insertAdjacentHTML(
-        "beforeend",
-        `<p class="input">${words}</p>`
-      )
-    }
+    const words1 = inputsindiv1.value;
+    const words2 = inputsindiv2.value;
+    previewimgdiv.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="input1" id="input">${words1}</p>
+      <p class="input2" id="input">${words2}</p>`
     );
-  }
-  else {
-    alert("test");
+  } else {
+    alert("No image detected");
   }
 }
 
 function runningputinputsonimg() {
   const btn = document.querySelector(".printinput");
-    btn.addEventListener("click", function () {
-      const oldtext = document.querySelectorAll(".input");
-      const oldtextArray = Array.from(oldtext);
-      oldtextArray.forEach((oldtext) => {
-      if (oldtext){
+  btn.addEventListener("click", function () {
+    const oldtext = document.querySelectorAll("#input");
+    const oldtextArray = Array.from(oldtext);
+    oldtextArray.forEach((oldtext) => {
+      if (oldtext) {
         oldtext.remove();
       }
-    })
+    });
     putinputonimg();
-    })
+  });
 }
 
 FirstInputs();
@@ -168,7 +182,9 @@ runningputinputsonimg();
 function filtering(btncategory) {
   const templates = document.querySelectorAll(".templateselect");
   templates.forEach((template) => {
-    const templateCategory = template.getAttribute("data-category").toLowerCase();
+    const templateCategory = template
+      .getAttribute("data-category")
+      .toLowerCase();
     if (templateCategory === btncategory || btncategory === "all") {
       template.style.display = "";
     } else {
--- app/src/modesbtn.js
+++ app/src/modesbtn.js
@@ -1,20 +1,18 @@
-export function setupmodes() {
-    const htmlEL = document.documentElement;
-    const lighttodarkswitch = document.querySelector(".lighttodarkswitch");
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        htmlEL.classList.add("dark");
-    }
-    else {
-        htmlEL.classList.add("light");
-    }
-    lighttodarkswitch.addEventListener("click", () => {
-        if (htmlEL.classList.contains("light")) {
-            htmlEL.classList.remove("light");
-            htmlEL.classList.add("dark");
-        }
-        else {
-            htmlEL.classList.remove("dark");
-            htmlEL.classList.add("light");
-        }
-    })
-}
+// export function setupmodes() {
+//   const htmlEL = document.documentElement;
+//   const lighttodarkswitch = document.querySelector(".lighttodarkswitch");
+//   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+//     htmlEL.classList.add("dark");
+//   } else {
+//     htmlEL.classList.add("light");
+//   }
+//   lighttodarkswitch.addEventListener("click", () => {
+//     if (htmlEL.classList.contains("light")) {
+//       htmlEL.classList.remove("light");
+//       htmlEL.classList.add("dark");
+//     } else {
+//       htmlEL.classList.remove("dark");
+//       htmlEL.classList.add("light");
+//     }
+//   });
+// }
